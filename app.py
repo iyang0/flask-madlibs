c919e43dd2035edacdb187fb3cf8bd5cba762bee
magus0 <harvestashya@gmail.com>
2021-07-07 16:10:03 -0700

    base prompts

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def root():
+    story_prompts = silly_story.prompts
+    print(story_prompts)
+    return render_template(
+        "questions.html",
+        prompts=story_prompts)
+        
+@app.route('/results')
+def story():
+    return render_template(
+        "story.html"
+        )

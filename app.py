0e149f5483d069222a4f9f0fc97b442ca25b9538
magus0 <harvestashya@gmail.com>
2021-07-07 18:03:50 -0700

    we're brilliant

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import STORIES
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -10,20 +11,25 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def root():
-    """Prompts user for input text"""
-    story_prompts = silly_story.prompts
+    """base path, asks user for what story they want."""
+    return render_template("story_picker.html")
+
+@app.route('/<story>')
+def prompt(story):
+    """Prompts user for input text of prompts"""
+    story_prompts = STORIES[story].prompts
 
     return render_template(
         "questions.html",
+        madlib=story,
         prompts=story_prompts)
         
-@app.route('/results')
-def story():
+@app.route('/<story>/results')
+def story(story):
     """Generates a story from the input and places it in the DOM"""
 
     story_answers = request.args
-
-    story_template = silly_story.generate(story_answers)
+    story_template = STORIES[story].generate(story_answers)
 
     return render_template(
         "story.html",

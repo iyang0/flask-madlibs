3308a1310fe74013771c9e29db4804cf88ce9677
lillianlakes <lillian.lakes@gmail.com>
2021-07-07 17:15:11 -0700

    adds functionality to show resulting story for answers

--- app.py
+++ app.py
@@ -10,14 +10,22 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def root():
+    """Prompts user for input text"""
     story_prompts = silly_story.prompts
-    print(story_prompts)
+
     return render_template(
         "questions.html",
         prompts=story_prompts)
         
 @app.route('/results')
 def story():
+    """Generates a story from the input and places it in the DOM"""
+
+    story_answers = request.args
+
+    story_template = silly_story.generate(story_answers)
+
     return render_template(
-        "story.html"
+        "story.html",
+        final_story = story_template
         )
